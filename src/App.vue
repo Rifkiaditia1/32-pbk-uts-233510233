f8d8800a5bce5f29815b0fd61ab6e9e7d3b3c3ac
Rifkiaditia1 <rifkididit205@gmail.com>
2025-05-06 23:08:47 +0700

    Menambahkan dan melihat kegiatan

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,60 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="app">
+    <div class="card">
+      <h1 class="title">📝 Daftar Kegiatan</h1>
+
+      <div class="input-container">
+        <input
+          v-model="newTask"
+          @keyup.enter="addTask"
+          placeholder="Tambah kegiatan baru..."
+          class="input"
+        />
+        <button @click="addTask" class="btn">Tambah</button>
+      </div>
+
+      
+
+      <ul class="task-list">
+        <li
+          v-for="(task, index) in filteredTasks"
+          :key="index"
+          
+        >
+          
+          <span class="task-text">{{ task.text }}</span>
+          
+        </li>
+      </ul>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+export default {
+  data() {
+    return {
+      newTask: '',
+      tasks: [],
+      
+    }
+  },
+  computed: {
+    filteredTasks() {
+      return this.showOnlyIncomplete
+        ? this.tasks.filter(task => !task.done)
+        : this.tasks;
+    }
+  },
+  methods: {
+    addTask() {
+      if (this.newTask.trim()) {
+        this.tasks.push({ text: this.newTask.trim(), done: false });
+        this.newTask = '';
+      }
+    
+    }
+  }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+</script>
+
